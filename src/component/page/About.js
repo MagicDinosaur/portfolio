e90337579e2c0b60ebb2d293fa6aa514346dc040
MagicDinosaur <phamvduc2112@gmail.com>
2022-12-25 00:31:24 -0500

    Feat!: About Page - Timeline

--- src/component/page/About.js
+++ src/component/page/About.js
@@ -1,16 +1,66 @@
 
 import {useRecoilState} from "recoil";
-import {useEffect,useState} from "react";
-import {useParams} from "react-router-dom";
-
 import './about.css';
 import { about as portfolioAbout} from "../../store/about";
-
+import { useState } from "react";
+import TrackVisibility from 'react-on-screen';
 export default function About() {
 
     const [usePortfolioAbout, setPortfolioAbout] = useRecoilState(portfolioAbout);
+    const [show, setShow] = useState(true);
+    // console.log(usePortfolioAboutTimeLine);
+    const Work = () => {
+        return (
+        <ul class="timeline">
+            {Object.keys(usePortfolioAbout['work']).map(key => (
 
-    // const [useItem, setItem] = useState(usePortfolioAbout[useParams().key]);
+                <li>
+                    <div class={parseInt(key) % 2 == 0 ? "direction-r" : "direction-l"}  >
+
+                        <div class="flag-wrapper">
+                            <span class="flag">{usePortfolioAbout['work'][key]['company']}</span>
+                            <span class="time-wrapper"><span class="time">{usePortfolioAbout['work'][key]['time']}</span></span>
+                        </div>
+
+                        <div class="desc">
+                            <img className="img-timeline" src={usePortfolioAbout['work'][key]['logo']}></img>
+                            <p> {usePortfolioAbout['work'][key]['description']}  </p>
+                        </div>
+                    </div>
+                </li>
+            ))
+            }
+        </ul>
+        )
+    }
+    const Education = () => {
+        return (
+            <ul class="timeline">
+                {Object.keys(usePortfolioAbout['education']).map(key => (
+
+                    <li>
+                        <div class={parseInt(key) % 2 == 0 ? "direction-r" : "direction-l"}  >
+
+                            <div class="flag-wrapper">
+                                <span class="flag">{usePortfolioAbout['education'][key]['name']}</span>
+                                <span class="time-wrapper"><span class="time">{usePortfolioAbout['education'][key]['time']}</span></span>
+                            </div>
+
+                            <div class="desc">
+                                {/* <img className="img-timeline" src={usePortfolioAbout['work'][key]['logo']}></img> */}
+                                <p> {usePortfolioAbout['education'][key]['description']}  </p>
+                            </div>
+                        </div>
+                    </li>
+                ))
+                }
+            </ul>
+        )
+    }
+
+    const handleTimeLine =(e) => {
+        setShow(!show);
+    }
 
     return (
         <div className="container-default w-container" >
@@ -24,30 +74,69 @@ export default function About() {
                 This is where you can find the infomation about my education and work experience, which is not available on my resume or my Linkedin. Thank you for coming
             </p>
 
-            <ul class="timeline">
-                {Object.keys(usePortfolioAbout['work']).map(key => (
-                   
-                    <li>
-                        <div class={parseInt(key) % 2 == 0 ? "direction-r" : "direction-l" }  >
-                        
-                            <div class="flag-wrapper">
-                                <span class="flag">{usePortfolioAbout['work'][key]['company']}</span>
-                                <span class="time-wrapper"><span class="time">{usePortfolioAbout['work'][key]['time']}</span></span>
-                            </div>
-                            
-                            <div class="desc">
-                                <img className="img-timeline" src={usePortfolioAbout['work'][key]['logo']}></img>
-                                <p> {usePortfolioAbout['work'][key]['description']}  </p>
-
-                            </div>
-                       
-                        
-                        
+            <br/>
+            {/* Education and Work timeline */}
+            <h4 style={{
+                    textAlign: "left",
+                }}> Education and Work timeline </h4>
+            
+            <div className="about-timeline-work">
+                <div className="about-timeline-work nav">
+                    <button className="about-timeline-work button" onClick={handleTimeLine}> Work </button>                
+                    <button className="about-timeline-work button" onClick={handleTimeLine}> Education </button>
+                </div>
+                <div className="divider"
+                style={{
+                    width: '80%',
+                    margin: 'auto',
+                }}></div>       
+                <TrackVisibility>
+                    {({ isVisible }) =>
+                        <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
+                            {show ? <Work /> : <Education />}
                         </div>
+                        }
+                </TrackVisibility>
+
+                {/* <ul class="timeline work">
+                    {Object.keys(usePortfolioAbout['work']).map(key => (
+
+                        <li>
+                            <div class={parseInt(key) % 2 == 0 ? "direction-r" : "direction-l"}  >
+
+                                <div class="flag-wrapper">
+                                    <span class="flag">{usePortfolioAbout['work'][key]['company']}</span>
+                                    <span class="time-wrapper"><span class="time">{usePortfolioAbout['work'][key]['time']}</span></span>
+                                </div>
+
+                                <div class="desc">
+                                    <img className="img-timeline" src={usePortfolioAbout['work'][key]['logo']}></img>
+                                    <p> {usePortfolioAbout['work'][key]['description']}  </p>
+                                </div>
+                            </div>
                         </li>
-                ))
-                }
-            </ul>
+                    ))
+                    }
+                </ul>
+                <ul class="timeline education" {...show ? "hidden" : "" }>
+                    {Object.keys(usePortfolioAbout['work']).map(key => (
+                        <li>
+                            <div class={parseInt(key) % 2 == 0 ? "direction-r" : "direction-l"}  >
+
+                                <div class="flag-wrapper">
+                                    <span class="flag">{usePortfolioAbout['work'][key]['company']}</span>
+                                    <span class="time-wrapper"><span class="time">{usePortfolioAbout['work'][key]['time']}</span></span>
+                                </div>
+                                <div class="desc">
+                                    <img className="img-timeline" src={usePortfolioAbout['work'][key]['logo']}></img>
+                                    <p> {usePortfolioAbout['work'][key]['description']}</p>
+                                </div>
+                            </div>
+                        </li>
+                    ))
+                    }
+                </ul> */}
+            </div>
 
 
         </div>
--- src/store/about.js
+++ src/store/about.js
@@ -32,7 +32,42 @@ export const about = atom({
             }
         },
         'education': {
-
+            '1': {
+                'name': 'Georgia State University',
+                'time': 'Aug 2020 – Current',
+                'location': 'Atlanta, GA',
+                'degree': 'Bachelor of Science in Computer Science',
+                'logo': 'https://i.imgur.com/4Z7ZQ9M.png',
+                'description': 'I am currently pursuing a Bachelor of Science in Computer Science at Georgia State University. \
+                                Six time award winner of Student President\'s List. GPA: 4.17',
+            },
+            '2': {
+                'name': 'CodePath SWE 02 - 03',
+                'time': 'May 2022 – Aug 2022',
+                'location': 'Remote',
+                'degree': 'Software Engineering Advanced Track',
+                'logo': 'https://i.imgur.com/4Z7ZQ9M.png',
+                'description': 'I was enrolled in CodePath\'s Software Engineering Advanced Track, a 16-week program about data structure and algorithms concept, soft skills'
+            },
+            '3' : {
+                'name': 'TRENDS center',
+                'time': 'May 2022 – Aug 2022',
+                'location': 'Atlanta, GA',
+                'degree': 'NeuralImaging and Machine Learning Research',
+                'logo': 'https://i.imgur.com/4Z7ZQ9M.png',
+                'description': "I was a member of TRENDS (The Georgia State University/Georgia Institute of Technology/Emory University)'s Center \
+                 for Translational Research in Neuroimaging and Data Science)'s research. Research Team: Independent Vector Analysis \
+                    Submitted paper at SCD 2022: NeuroImaging at Scale: Distributed Computing for Detection of Independent Subspaces in IVA"
+            },
+            '4': {
+                'name': 'Hanoi Univeristy of Science and Technology',
+                'time': 'Aug 2018 – Jan 2019',
+                'location': 'Hanoi, Vietnam',
+                'degree': 'Bachelor of Science in Computer Science',
+                'logo': 'https://i.imgur.com/4Z7ZQ9M.png',
+                'description': 'With an outstanding National High School Graduation Exam score \
+                (top 0.00035% over the whole country), I was admitted to HUST - Ranked 1st in the country school list, Computer Science major'
+            }
 
 
         }
