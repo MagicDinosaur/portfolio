13368f7708a2b84f15193eaf4c39aeed14ff99d6
MagicDinosaur <phamvduc2112@gmail.com>
2022-12-26 16:00:52 -0500

    Feat!: About Page - files

--- src/component/page/About.js
+++ src/component/page/About.js
@@ -4,11 +4,12 @@ import './about.css';
 import { about as portfolioAbout} from "../../store/about";
 import { useState } from "react";
 import TrackVisibility from 'react-on-screen';
+import { Divider, Button } from "antd";
+import { DownloadOutlined } from '@ant-design/icons';
 export default function About() {
 
     const [usePortfolioAbout, setPortfolioAbout] = useRecoilState(portfolioAbout);
     const [show, setShow] = useState(true);
-    // console.log(usePortfolioAboutTimeLine);
     const Work = () => {
         return (
         <ul class="timeline">
@@ -76,11 +77,12 @@ export default function About() {
 
             <br/>
             {/* Education and Work timeline */}
+            <div className="about-timeline-work">
             <h4 style={{
                     textAlign: "left",
                 }}> Education and Work timeline </h4>
-            
-            <div className="about-timeline-work">
+            <p> My journey in education and professonal work could be seen below</p>
+            <br/>
                 <div className="about-timeline-work nav">
                     <button className="about-timeline-work button" onClick={handleTimeLine}> Work </button>                
                     <button className="about-timeline-work button" onClick={handleTimeLine}> Education </button>
@@ -97,48 +99,26 @@ export default function About() {
                         </div>
                         }
                 </TrackVisibility>
-
-                {/* <ul class="timeline work">
-                    {Object.keys(usePortfolioAbout['work']).map(key => (
-
-                        <li>
-                            <div class={parseInt(key) % 2 == 0 ? "direction-r" : "direction-l"}  >
-
-                                <div class="flag-wrapper">
-                                    <span class="flag">{usePortfolioAbout['work'][key]['company']}</span>
-                                    <span class="time-wrapper"><span class="time">{usePortfolioAbout['work'][key]['time']}</span></span>
-                                </div>
-
-                                <div class="desc">
-                                    <img className="img-timeline" src={usePortfolioAbout['work'][key]['logo']}></img>
-                                    <p> {usePortfolioAbout['work'][key]['description']}  </p>
-                                </div>
-                            </div>
-                        </li>
-                    ))
-                    }
-                </ul>
-                <ul class="timeline education" {...show ? "hidden" : "" }>
-                    {Object.keys(usePortfolioAbout['work']).map(key => (
-                        <li>
-                            <div class={parseInt(key) % 2 == 0 ? "direction-r" : "direction-l"}  >
-
-                                <div class="flag-wrapper">
-                                    <span class="flag">{usePortfolioAbout['work'][key]['company']}</span>
-                                    <span class="time-wrapper"><span class="time">{usePortfolioAbout['work'][key]['time']}</span></span>
-                                </div>
-                                <div class="desc">
-                                    <img className="img-timeline" src={usePortfolioAbout['work'][key]['logo']}></img>
-                                    <p> {usePortfolioAbout['work'][key]['description']}</p>
-                                </div>
-                            </div>
-                        </li>
-                    ))
-                    }
-                </ul> */}
             </div>
-
-
+            <Divider/>
+            {/* Additional file */}
+            <div className="addional-file"></div>
+            <h4 style={{
+                textAlign: "left",
+            }}> Additional Document</h4>
+            <p> If you are here, you're probably a recruiter, hiring manager, or, I hope so, one of my future colleagues. You should have my resume in hand ^^. 
+                So, holla, here is my latest transcript for those who are curious about it </p>
+            <br/>
+            <Button type="primary" shape="round" icon={<DownloadOutlined />} size={32} 
+                style={{
+                    background: "grey", 
+                    display : "block",
+                    margin: "auto",
+                    width: "200px",
+                     }} 
+                        href="https://drive.google.com/file/d/11BWPAE-wbWtYtSHdM9cKmNLaTkvNtjFl/view?usp=sharing" 
+                        target="_blank">  Download my transcript</Button>
+            <br />
         </div>
     );
 }
